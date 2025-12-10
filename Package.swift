20f2358b79771fc91e618d3d2f235ccbe590098b
Momo L'As <momolas@users.noreply.github.com>
2025-12-10 18:40:04 +0100

    Rename package from 'Swift-OBD2' to 'SwiftOBD2'

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swift-OBD2",
+    name: "SwiftOBD2",
     platforms: [
         .iOS("26.0"),
         .macOS("16.0")
@@ -12,26 +12,19 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Swift-OBD2",
-            targets: ["Swift-OBD2"]
+            name: "SwiftOBD2",
+            targets: ["SwiftOBD2"]
         )
     ],
-//    dependencies: [
-//        // ...
-//        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2"),
-//    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Swift-OBD2"
-//            plugins: [
-//                .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
-//            ]
+            name: "SwiftOBD2"
         ),
         .testTarget(
-            name: "Swift-OBD2Tests",
-            dependencies: ["Swift-OBD2"]
+            name: "SwiftOBD2Tests",
+            dependencies: ["SwiftOBD2"]
         )
     ]
 )
